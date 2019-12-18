2fc7e85085cae956d15cda2745877a761c84e87a
Chris Ziogas <ziogaschr@gmail.com>
2019-12-18 14:52:12 +0200

    Add Test_TableOrderingBigInt

--- db_test.go
+++ db_test.go
@@ -832,6 +832,56 @@ func Test_TableOrdering(t *testing.T) {
 	}
 }
 
+func Test_TableOrderingBigInt(t *testing.T) {
+	db, err := Open(tempfile(), 0, nil)
+	defer os.Remove(db.GetPath())
+	if err != nil || db == nil {
+		t.Fatal("Failed to open db", err)
+	}
+
+	type Witness struct {
+		Id     common.Address
+		Payout *big.Int
+	}
+
+	const WitnessesTable string = "Witnesses"
+
+	db.CreateTable(WitnessesTable, &Witness{})
+	db.CreateIndex(IndexField{
+		Table: WitnessesTable,
+		Field: "Payout",
+	})
+
+	snap := db.GetRootSnapshot()
+
+	if err := snap.InsertObj(WitnessesTable, &Witness{
+		Id:     common.HexToAddress("0x02d4697643696464de19438f581519cb11ca750b"),
+		Payout: big.NewInt(355),
+	}); err != nil {
+		t.Fatal("Failed to insert row error:", err)
+	}
+
+	if err := snap.InsertObj(WitnessesTable, &Witness{
+		Id:     common.HexToAddress("0x3b6b4e86529e0e19dc32ff78a56c344c3a6bf895"),
+		Payout: big.NewInt(255),
+	}); err != nil {
+		t.Fatal("Failed to insert row error:", err)
+	}
+
+	var w Witness
+
+	orderClause, _ := snap.OrderParser([]byte("Payout DESC"))
+	iter, err := snap.Select(WitnessesTable, nil, orderClause)
+
+	lastPayout := big.NewInt(1000)
+	for iter.Next(&w) {
+		fmt.Println(w.Id.Hex(), w.Payout)
+		if w.Payout.Cmp(lastPayout) > 0 {
+			t.Fatal("Improper ordering")
+		}
+		lastPayout = new(big.Int).Set(w.Payout)
+	}
+}
 func Test_TableDuplicates(t *testing.T) {
 	db, err := Open(tempfile(), 0, nil)
 	defer os.Remove(db.GetPath())
